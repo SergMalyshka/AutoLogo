d4fa1a5974ae9a5145707aa5d3f8b1259de36458
Sergei Malyshka <104466946+SergMalyshka@users.noreply.github.com>
2024-03-02 18:50:24 -0500

    Comment commit
    added comments to most of the js classes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+//all the imports
 const inquirer = require('inquirer');
 const Circle = require("./lib/circle.js")
 const Square = require("./lib/square.js")
 const Triangle = require("./lib/triangle.js")
 const fs = require('fs')
 
+//inquirer questions required for the generation
 function init() {
     inquirer
         .prompt([
@@ -30,23 +32,28 @@ function init() {
             },
         ])
         .then((response) =>
+            //sending code to the create the shape based on the provided info
             generateShape(response)
         );
 }
 
 function generateShape(data) {
     if (data.shape === "Circle") {
+        //if the shape is a circle make a new circle and populate it with the proper data
         const shape = new Circle();
         shape.setShapeAndColor(data.shapeColor)
         shape.setText(data.content, data.textColor)
+        //generate the file based on the new shape, repeated in other if else statements
         generateFile(shape)
 
     } else if (data.shape == "Square") {
+        //if the shape is a sqaure make a new square and populate it with the proper data
         const shape = new Square();
         shape.setShapeAndColor(data.shapeColor)
         shape.setText(data.content, data.textColor)
         generateFile(shape)
     } else {
+        //if its neither of the first two it has to be a triangle
         const shape = new Triangle();
         shape.setShapeAndColor(data.shapeColor)
         shape.setText(data.content, data.textColor)
@@ -55,12 +62,13 @@ function generateShape(data) {
 }
 
 function generateFile(shape) {
+    //dynamically create the SVG data
     const fileData =
         `${shape.header} \n
     ${shape.shape} \n
     ${shape.text} \n
     ${shape.footer}`
-
+    //write the data into the file, throw an error if the error is caught
     fs.writeFile("logo.svg", fileData, (err) =>
         err ? console.error(err) : console.log('Generated logo.svg'))
 }
